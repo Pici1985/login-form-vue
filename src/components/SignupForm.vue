afcb3bc7b9ef143d1284e50040ce66828446232e
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-05-11 19:50:45 +0100

    Initialization
    Initial commit and cleanup, basic form

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <form>
+            <label>Email:</label>
+            <input type="email" required v-model="email">
+            <br>
+            <label>Password:</label>
+            <input type="password" required v-model="password">
+
+            <label>Role:</label>
+            <br>
+            <select v-model="role">
+                <option value="Developer">Web Developer</option>
+                <option value="Designer">Web Desginer</option>
+            </select>
+        </form>
+        <p> Your email is: {{ email }} </p>
+        <p> Password is: {{ password }}</p>
+        <p> Your Job title is: {{ role }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            role: '',
+        }
+    }
+}
+</script>
+
+<style>
+    form {
+        max-width: 420px;
+        margin: 30px auto;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label {
+        color: #aaa;
+        display: inline-block;
+        margin: 20px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input, select {
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// https://www.youtube.com/watch?v=ixOcve5PX-Q&t=1s
+// 13:48-nal
